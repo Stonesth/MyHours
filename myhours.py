bf53dce6494d36fceef842ad0d031193a9b53e4e
Stonesth <pierre.thonon@gmail.com>
2021-08-31 15:50:03 +0200

    New version of MyHours => need to change startTrackWithDescription

--- myhours.py
+++ myhours.py
@@ -47,7 +47,7 @@ def startTrackWithDescription(jira, description, epic_link) :
     startTrack()
 
     # Click on the current run 
-    timeStep1 = tools.driver.find_element_by_xpath('//*[@id="trackPage"]/div[4]/div/div[2]')
+    timeStep1 = tools.driver.find_element_by_xpath('//*[@id="trackPage"]/div[4]/div/div[2]/div/log-display/div/div[1]/div/div[1]/log-details-display')
 
     timeStep1.click()
     modifyTrack(jira, description, epic_link)
