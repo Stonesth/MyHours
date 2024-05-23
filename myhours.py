6b8e3ae14dfaa740d2843a3f1e92e873062d7fa3
Stonesth <pierre.thonon@gmail.com>
2024-05-23 15:19:08 +0200

    Change when I used waitLoadingPageByXPATH

--- myhours.py
+++ myhours.py
@@ -179,10 +179,11 @@ def modifyTrack(jira, description, epic_link) :
         tools.waitLoadingPageByXPATH2(dealy_properties, '//*[@id="logFormEditLogProjectInput"]/mh-select-box-toolbar-actions/dx-select-box/div/div[1]/div/div/div/dx-text-box/div/div[1]/input')  
         time.sleep(2)
         projectInput = tools.driver.find_element(By.XPATH, '//*[@id="logFormEditLogProjectInput"]/mh-select-box-toolbar-actions/dx-select-box/div/div[1]/div/div/div/dx-text-box/div/div[1]/input')
-        projectInput.send_keys(epic_link + Keys.ENTER)
+        for char in epic_link:
+            projectInput.send_keys(char)
         time.sleep(1)
-        # projectInput = tools.driver.find_element(By.XPATH, '//*[@id="logFormEditLogProjectInput"]/mh-select-box-toolbar-actions/dx-select-box/div/div[1]/div/div/div/dx-text-box/div/div[1]/input')
-        # projectInput.send_keys(Keys.ENTER)
+        projectInput = tools.driver.find_element(By.XPATH, '//*[@id="logFormEditLogProjectInput"]/mh-select-box-toolbar-actions/dx-select-box/div/div[1]/div/div/div/dx-text-box/div/div[1]/input')
+        projectInput.send_keys(Keys.ENTER)
 
         time.sleep(2)
 
